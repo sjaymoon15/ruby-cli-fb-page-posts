299c82bbf0d324ce0e6656e574b7f0f51c8222ce
Seungjae Moon <sjaymoon15@gmail.com>
2018-03-04 16:20:48 -0800

    Create a helper module and use its methods to make code more readable

--- lib/fb_pages_most_liked_posts.rb
+++ lib/fb_pages_most_liked_posts.rb
@@ -18,82 +18,80 @@ module FbPagesMostLikedPosts
       access_token = oauth.get_app_access_token
       client = Koala::Facebook::API.new(access_token)
 
-      if options[:MAX_POSTS_PER_PAGE].to_i <= 100
-        max_posts_per_page = options[:MAX_POSTS_PER_PAGE]
-      else
-        max_posts_per_page = 100
-        Formatador.display_line("[green]Maximum number of posts you can search is 100[/]")
-      end
-      Formatador.display_line("[green]Searching #{max_posts_per_page} posts for each page...[/]")
-
-      top_posts_count = options[:TOP_POSTS_COUNT].to_i <= max_posts_per_page.to_i ?
-                          options[:TOP_POSTS_COUNT].to_i :
-                          max_posts_per_page.to_i
-      Formatador.display_line("[green]Showing #{top_posts_count.to_s} posts for each page...[/]")
-      puts ''
-
+      max_posts_per_page = Helper.get_max_posts_per_page(options[:MAX_POSTS_PER_PAGE])
+      top_posts_count = Helper.get_top_posts_count(options[:TOP_POSTS_COUNT], max_posts_per_page)
       params = "posts?fields=likes.summary(true),message,created_time&limit=#{max_posts_per_page}"
 
       coca_cola_posts = client.get_connection('CocaColaUnitedStates', params)
       fc_barcelona_posts = client.get_connection('fcbarcelona', params)
       whole_foods_posts = client.get_connection('WholeFoods', params)
 
-      coca_cola_posts_sorted = coca_cola_posts.sort { |a, b|
-        a['likes']['summary']['total_count'] <=> b['likes']['summary']['total_count']
-      }.reverse
+      coca_cola_posts_sorted = Helper.sort_posts_by_likes(coca_cola_posts)
+      fc_barcelona_posts_sorted = Helper.sort_posts_by_likes(fc_barcelona_posts)
+      whole_foods_posts_sorted = Helper.sort_posts_by_likes(whole_foods_posts)
 
-      fc_barcelona_posts_sorted = fc_barcelona_posts.sort { |a, b|
-        a['likes']['summary']['total_count'] <=> b['likes']['summary']['total_count']
-      }.reverse
+      coca_cola_posts_result = Helper.generate_posts_structure(coca_cola_posts_sorted, top_posts_count)
+      fc_barcelona_posts_result = Helper.generate_posts_structure(fc_barcelona_posts_sorted, top_posts_count)
+      whole_foods_posts_result = Helper.generate_posts_structure(whole_foods_posts_sorted, top_posts_count)
 
-      whole_foods_posts_sorted = whole_foods_posts.sort { |a, b|
-        a['likes']['summary']['total_count'] <=> b['likes']['summary']['total_count']
-      }.reverse
-
-      coca_cola_posts_result = []
-      coca_cola_posts_sorted.each_with_index {| post, index |
-        if index < top_posts_count
-          coca_cola_posts_result << {
-            id: post['id'],
-            num_of_likes: post['likes']['summary']['total_count'],
-            created_time: post['created_time']
-          }
-        end
-      }
-
-      fc_barcelona_posts_result = []
-      fc_barcelona_posts_sorted.each_with_index {| post, index |
-        if index < top_posts_count
-          fc_barcelona_posts_result << {
-            id: post['id'],
-            num_of_likes: post['likes']['summary']['total_count'],
-            created_time: post['created_time']
-          }
-        end
-      }
-
-      whole_foods_posts_result = []
-      whole_foods_posts_sorted.each_with_index {| post, index |
-        if index < top_posts_count
-          whole_foods_posts_result << {
-            id: post['id'],
-            num_of_likes: post['likes']['summary']['total_count'],
-            created_time: post['created_time']
-          }
-        end
-      }
-
-      Formatador.display_line('[green]Coca Cola United States[/]')
-      Formatador.display_table(coca_cola_posts_result, [:num_of_likes, :id, :created_time])
-      puts
-
-      Formatador.display_line('[green]FC Barcelona[/]')
-      Formatador.display_table(fc_barcelona_posts_result, [:num_of_likes, :id, :created_time])
-      puts
-
-      Formatador.display_line('[green]Whole Foods[/]')
-      Formatador.display_table(whole_foods_posts_result, [:num_of_likes, :id, :created_time])
-      puts
+      Helper.print_results(coca_cola_posts_result, fc_barcelona_posts_result, whole_foods_posts_result)
     end
+  end
+end
+
+module Helper
+  def self.get_max_posts_per_page(max_posts_per_page_input)
+    if max_posts_per_page_input.to_i <= 100
+      max_posts_per_page = max_posts_per_page_input
+    else
+      max_posts_per_page = 100
+      Formatador.display_line("[green]Maximum number of posts you can search is 100[/]")
+    end
+    Formatador.display_line("[green]Searching #{max_posts_per_page} posts for each page...[/]")
+    puts ''
+    return max_posts_per_page
+  end
+
+  def self.get_top_posts_count(top_posts_count_input, max_posts_per_page)
+    top_posts_count = top_posts_count_input.to_i <= max_posts_per_page.to_i ?
+                        top_posts_count_input.to_i :
+                        max_posts_per_page.to_i
+    Formatador.display_line("[green]Showing #{top_posts_count.to_s} posts for each page...[/]")
+    puts ''
+    return top_posts_count
+  end
+
+  def self.sort_posts_by_likes(posts)
+    posts.sort { |a, b|
+      a['likes']['summary']['total_count'] <=> b['likes']['summary']['total_count']
+    }.reverse
+  end
+
+  def self.generate_posts_structure(posts, top_posts_count)
+    posts_result = []
+    posts.each_with_index {| post, index |
+      if index < top_posts_count
+        posts_result << {
+          id: post['id'],
+          num_of_likes: post['likes']['summary']['total_count'],
+          created_time: post['created_time']
+        }
+      end
+    }
+    return posts_result
+  end
+
+  def self.print_results(coca_cola_posts_result, fc_barcelona_posts_result, whole_foods_posts_result)
+    Formatador.display_line('[green]Coca Cola United States[/]')
+    Formatador.display_table(coca_cola_posts_result, [:num_of_likes, :id, :created_time])
+    puts ''
+
+    Formatador.display_line('[green]FC Barcelona[/]')
+    Formatador.display_table(fc_barcelona_posts_result, [:num_of_likes, :id, :created_time])
+    puts ''
+
+    Formatador.display_line('[green]Whole Foods[/]')
+    Formatador.display_table(whole_foods_posts_result, [:num_of_likes, :id, :created_time])
+    puts ''
   end
 end
